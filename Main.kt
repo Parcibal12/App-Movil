f9f87cc2e648dbafc4202eda82cd1cfb1b912b01
Parcibal12 <danielirigoyen984@gmail.com>
2025-09-06 08:46:48 -0400

    Añade variables con datos personales y saludos

--- Main.kt
+++ Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Inicio del programa.")
+    val nombreCompleto = "Daniel Irigoyen Miranda"
+    val nombreMateria = "Aplicaciones Móviles"
+    val peso = 54
+    val altura = 1.68
+    val edad = 20
+    
+
+    val edadEsPar: Boolean = (edad % 2 == 0)
+
+    val genero = "Masculino"
+
+    println("Hola, mi nombre es $nombreCompleto.")
+    println("Estoy en la materia de $nombreMateria.")
+    println("Mis datos son: Peso: ${peso}kg, Altura: ${altura}m, Edad: $edad años.")
+    println("Mi edad es par? $edadEsPar")
+    println("Mi género es: $genero")
 }
