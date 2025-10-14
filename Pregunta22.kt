86e7b0ac1bdd16d3db921cfbf0ba8dc63785b990
Parcibal12 <danielirigoyen984@gmail.com>
2025-10-14 18:59:30 -0400

    Enunciado del ejercicio 22 con ejemplo

---
+++ Pregunta22.kt
@@ -0,0 +1,16 @@
+/*
+EJERCICIO 22:
+
+22. Número espejo (10 puntos):
+Instrucciones:
+Crea un programa que determine si un número enetero es un número espejo
+(palíndromo), es decir, que se lee igual al derecho y al revés.
+
+Ejemplo: 121, 1331, 909 son números espejo.
+Restricciones: Sin usar conversiones de tipo
+    Entrada: 1221
+    Salida: Es un número espejo
+
+    Entrada: 1234
+    Salida: No es un número espejo
+*/
