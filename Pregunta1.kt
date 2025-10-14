a6633f824d6d5a2f9cebb247481762f8e9121803
Parcibal12 <danielirigoyen984@gmail.com>
2025-10-14 18:10:52 -0400

    Solución con error del examen para Pregunta 16

--- Pregunta1.kt
+++ Pregunta1.kt
@@ -1,6 +1,25 @@
-/* 
-16. Manejo de Nulos (5 puntos): Corrige este código para manejar valores nulos:
-val email: String? = null
-val dominio = email.substringAfter("@")
+/*
+ * EJERCICIO 16:
+ * 
+ * 16. Manejo de Nulos (5 puntos): Corrige este código para manejar valores nulos:
+ * val email: String? = null
+ * val dominio = email.substringAfter("@")
+ * println("Dominio: $dominio")
+ *
+ * EJEMPLOS DE ENTRADA Y SALIDA
+ *
+ *
+ * CASO 1: Entrada con un email válido
+ * - Entrada: val email: String? = "estudiante@universidad.com"
+ * - Salida esperada: Dominio: universidad.com
+ *
+ * CASO 2: Entrada con un valor nulo (el caso a corregir)
+ * - Entrada: val email: String? = null
+ * - Salida esperada (después de la corrección):
+ * "Dominio: No especificado"
+ *
+ */
+// Solución del examen (con error)
+var email: String? = null
+var dominio = email.substringAfter("@")
 println("Dominio: $dominio")
-*/
