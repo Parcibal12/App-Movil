d86cdb321df0ab07960d5f524bccec7707659c49
Parcibal12 <danielirigoyen984@gmail.com>
2025-10-14 18:04:45 -0400

    Enunciado del Ejercicio 1 con ejemplo

---
+++ Pregunta1.kt
@@ -0,0 +1,6 @@
+/* 
+16. Manejo de Nulos (5 puntos): Corrige este código para manejar valores nulos:
+val email: String? = null
+val dominio = email.substringAfter("@")
+println("Dominio: $dominio")
+*/
