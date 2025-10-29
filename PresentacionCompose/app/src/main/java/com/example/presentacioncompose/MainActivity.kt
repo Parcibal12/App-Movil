31a800407b061dbe2d56b511dc10df9429a1b9e7
Parcibal12 <danielirigoyen984@gmail.com>
2025-10-29 13:27:59 -0400

    Agrega ejemplo de layout Column

--- PresentacionCompose/app/src/main/java/com/example/presentacioncompose/MainActivity.kt
+++ PresentacionCompose/app/src/main/java/com/example/presentacioncompose/MainActivity.kt
@@ -1,47 +1,57 @@
-package com.example.presentacioncompose
+package com.example.presentacionapp
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.presentacioncompose.ui.theme.PresentacionComposeTheme
+import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
+
+//Ejemplo 1: Column
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PresentacionComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MiEjemploDeColumn()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MiEjemploDeColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "hola")
+        Text(text = "hdp")
+        Text(text = "qta")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "1. Column")
 @Composable
-fun GreetingPreview() {
-    PresentacionComposeTheme {
-        Greeting("Android")
-    }
+fun PreviewColumn() {
+    MiEjemploDeColumn()
 }
